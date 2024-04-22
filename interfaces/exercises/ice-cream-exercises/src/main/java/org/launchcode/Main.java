929f327b19ef6c9131e6107a1d393ecea01d9a49
Trevor Gruber <gruber.trevor9281@gmail.com>
2024-04-22 17:27:21 -0500

    Chapter 8 exercises complete

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -1,17 +1,25 @@
 package org.launchcode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
-
+      
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
-
+        flavors.sort(new FlavorComparator());
+        
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
-
+        cones.sort(new ConeComparator());
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        for (Flavor flavor : flavors) {
+            System.out.println("Flavor: " + flavor.getName());
+        }
+        for (Cone cone : cones) {
+            System.out.println("Cone: " + cone.getName() + " - Cost: $" + cone.getCost());
+        }
     }
 }
