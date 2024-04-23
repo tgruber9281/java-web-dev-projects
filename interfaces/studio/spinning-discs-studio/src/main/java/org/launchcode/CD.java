9dca9de617de21c91c251e78bc6bdc23e9d5994b
Trevor Gruber <gruber.trevor9281@gmail.com>
2024-04-22 20:54:23 -0500

    Chapter 8 studio complete

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/CD.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/CD.java
@@ -1,6 +1,6 @@
 package org.launchcode;
 
-public class CD {
+public class CD extends BaseDisk implements OpticalDisk {
     // TODO: Implement your custom interface.
 
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
